aa3569643339601a0dfe0497d1c70ec71946d5dd
cdavies2 <cdavies@student.sjny.edu>
2025-03-06 16:12:09 -0500

    Created unit test with both TFHE and OpenFHE

--- fhe_schemes.py
+++ fhe_schemes.py
@@ -4,6 +4,13 @@ import openfhe as ofhe
 import tfhe as tfhe
 import numpy as np
 
+from openfhe import(
+    CryptoContext,
+    CCParamsCKKSRNS,
+    GenCryptoContext,
+    PKESchemeFeature,
+    Plaintext
+)
 
 from tfhe.keys import(
     tfhe_decrypt,
@@ -25,6 +32,101 @@ class FHEScheme(ABC):
     def decrypt(self, key, ciphertext):
         pass
 
+# these abstract classes are used to provide consistency when using OpenFHE and TFHE
+class OpenFHE(FHEScheme):
+    def __init__(self):
+        self.cc=None # the cc value will be used for key generation, packing plaintext, encryption, and decryption
+        self.private=None
+        self.public=None
+        self.plaintext=None
+        self.plain_type=None
+        # maybe make a variable for input type, as different operations are performed on strings
+
+    #staticmethod is used here because these methods aren't bound to class objects   
+    @staticmethod
+    def setup_ckks(size):
+        mult_depth = 1 # the maximum number of multiplications an HE scheme is built to perform
+        scale_mod_size = 50 # the "key size", how many bits the key will be
+        batch_size = size # number of values to be encrypted
+        
+        parameters = CCParamsCKKSRNS() # create a CCParamsCKKSRNS object
+        parameters.SetMultiplicativeDepth(mult_depth) 
+        parameters.SetScalingModSize(scale_mod_size)
+        parameters.SetBatchSize(batch_size)
+        # multiplicative depth, key size, and batch size are set to the above values
+
+        cc = GenCryptoContext(parameters) # creates a CKKS instance using the parameters
+        # methods will be invoked using the above
+        cc.Enable(PKESchemeFeature.PKE) # PKE = Public Key Encryption
+        # Enabling this feature allows us to create public and private keys to use with our plaintext
+        return cc
+
+    @staticmethod
+    def nearest_power_padding(obj: str | list, obj_len):
+        # used with OpenFHE, as its batches must have lengths that are powers of two
+        exp=int(math.log2(obj_len)) # get log2 of the string's length
+        if (2**exp == obj_len):
+            return obj # if the length is already a power of two, return the string as is
+        else: # if it's not a power of two, find the closest power of 2
+            nearest_power=2**(exp + 1) # raise 2 to the power of the result of log2 plus 1
+            pad_len=nearest_power - obj_len # subtract the object length from the closest power of two
+            if type(obj)==str:
+                padded_str=obj + (" " * pad_len) # add the previously found difference to the string as padding
+                return padded_str
+            else: 
+                for i in range(pad_len):
+                    obj.append(" ")
+                return obj
+
+    @staticmethod
+    def remove_padding(padded, plain_len):
+        no_pad=padded[:plain_len]
+        return no_pad
+
+    def getKeys(self):
+        keys=self.cc.KeyGen()
+        private=keys.secretKey
+        public=keys.publicKey
+        # publicKey is used for encryption, secretKey is used for decryption
+        return private, public
+
+    def encrypt(self, plaintext):
+        self.plaintext=plaintext
+        self.plain_type=type(plaintext)
+        start_len=len(plaintext) 
+        plain_t=OpenFHE.nearest_power_padding(plaintext, start_len) # add padding to ensure length is a power of two
+
+        if self.plain_type==str:
+            plain_t=[(byte) for byte in plain_t.encode("utf-8")] # encode string characters into bytes using utf-8 format
+        cc = OpenFHE.setup_ckks(len(plain_t))  # create CKKS parameters for encryption
+        self.cc=cc # save crypto context for use in other methods
+
+        if (self.private==None or self.public==None): # if there are no keys
+            self.private, self.public=self.getKeys() # run the getKeys method
+        ptx=self.cc.MakeCKKSPackedPlaintext(plain_t) # converts the list of values to a plaintext object
+
+        cipher=self.cc.Encrypt(self.public, ptx) # encrypt the ciphertext
+        return cipher
+
+    def decrypt(self, ciphertext):
+        decrypt=self.cc.Decrypt(ciphertext, self.private)
+        vals=decrypt.GetRealPackedValue() # extract the plaintext values and put them in a list
+        final=[]
+        for i in vals:
+            rounded=round(i, 1)
+            final.append(rounded) # round the extracted values to one decimal place
+        if(self.plain_type==str):
+            encoded=[]
+            for i in final:
+                i_val=int(i)
+                encoded.append(i_val)
+            no_pad=OpenFHE.remove_padding(encoded, len(self.plaintext))
+            if(no_pad==[(byte)for byte in self.plaintext.encode("utf-8")]):
+                decrypt=self.plaintext
+                return decrypt
+        else:
+            return final
+        
 
 class TFHE(FHEScheme):
     def __init__(self):
--- fhe_test.py
+++ fhe_test.py
@@ -24,132 +24,6 @@ from openfhe import(
 str1 = "Hello" 
 str2 = "the magic words are squeamish ossifrage"
 
-
-def ints_to_bits(plain_t):
-    bit_list=[] # this object will be used if we have a list of integers
-    if type(plain_t)==list:
-        for i in plain_t:
-            bit=np.array([((i >> j) & 1 != 0) for j in range(8)]) # convert the integer to bits
-            # >> is the piecewise shift operator
-            # we choose 8 for our number of bits because we are encoding bytes for our strings/ints
-            bit_list.append(bit) # add the encoded integer to the bit_list
-        return bit_list
-    else:
-        bit = (np.array([((plain_t >> j) & 1 != 0) for j in range(8)])) # if one integer is sent, convert it to a bit array
-        return bit #return the array
-        
-
-def bits_to_ints(bit_list):
-    int_answer = 0  #this converts bits back to their initial number values
-    for i in range(8):
-        int_answer = int_answer | (bit_list[i] << i)
-    return int_answer  
-
-
-
-# def remove_padding(obj, og_length):
-#     no_pad=obj[:og_length]
-#     return no_pad
-        
-        
-
-class FHEScheme(ABC):
-    @abstractmethod
-    def getKeys(self):
-        pass
-
-    @abstractmethod
-    def encrypt(self, plaintext):
-        pass
-
-    @abstractmethod
-    def decrypt(self, key, ciphertext):
-        pass
-
-# these abstract classes are used to provide consistency when using OpenFHE and TFHE
-class OpenFHEScheme(FHEScheme):
-    def __init__(self):
-        self.cc=None # the cc value will be used for key generation, packing plaintext, encryption, and decryption
-        self.private=None
-        self.public=None
-        self.plaintext=None
-        self.plain_type=None
-        # maybe make a variable for input type, as different operations are performed on strings
-
-    #staticmethod is used here because these methods aren't bound to class objects   
-    @staticmethod
-    def setup_ckks(size):
-        mult_depth = 1 # the maximum number of multiplications an HE scheme is built to perform
-        scale_mod_size = 50 # the "key size", how many bits the key will be
-        batch_size = size # number of values to be encrypted
-        
-        parameters = CCParamsCKKSRNS() # create a CCParamsCKKSRNS object
-        parameters.SetMultiplicativeDepth(mult_depth) 
-        parameters.SetScalingModSize(scale_mod_size)
-        parameters.SetBatchSize(batch_size)
-        # multiplicative depth, key size, and batch size are set to the above values
-
-        cc = GenCryptoContext(parameters) # creates a CKKS instance using the parameters
-        # methods will be invoked using the above
-        cc.Enable(PKESchemeFeature.PKE) # PKE = Public Key Encryption
-        # Enabling this feature allows us to create public and private keys to use with our plaintext
-        return cc
-
-    @staticmethod
-    def nearest_power_padding(obj: str | list, obj_len):
-        # used with OpenFHE, as its batches must have lengths that are powers of two
-        exp=int(math.log2(obj_len)) # get log2 of the string's length
-        if (2**exp == obj_len):
-            return obj # if the length is already a power of two, return the string as is
-        else: # if it's not a power of two, find the closest power of 2
-            nearest_power=2**(exp + 1) # raise 2 to the power of the result of log2 plus 1
-            pad_len=nearest_power - obj_len # subtract the object length from the closest power of two
-            if type(obj)==str:
-                padded_str=obj + (" " * pad_len) # add the previously found difference to the string as padding
-                return padded_str
-            else: 
-                for i in range(pad_len):
-                    obj.append(" ")
-                return obj
-
-    def getKeys(self):
-        keys=self.cc.KeyGen()
-        private=keys.secretKey
-        public=keys.publicKey
-        # publicKey is used for encryption, secretKey is used for decryption
-        return private, public
-
-    def encrypt(self, plaintext):
-        self.plaintext=plaintext
-        self.plain_type=type(plaintext)
-        start_len=len(plaintext) 
-        plain_t=OpenFHEScheme.nearest_power_padding(plaintext, start_len) # add padding to ensure length is a power of two
-
-        if self.plain_type==str:
-            plain_t=[(byte) for byte in plain_t.encode("utf-8")] # encode string characters into bytes using utf-8 format
-        cc = OpenFHEScheme.setup_ckks(len(plain_t))  # create CKKS parameters for encryption
-        self.cc=cc # save crypto context for use in other methods
-
-        if (self.private==None or self.public==None): # if there are no keys
-            self.private, self.public=self.getKeys() # run the getKeys method
-        ptx=self.cc.MakeCKKSPackedPlaintext(plain_t) # converts the list of values to a plaintext object
-
-        cipher=self.cc.Encrypt(self.public, ptx) # encrypt the ciphertext
-        return cipher
-
-    def decrypt(self, ciphertext):
-        decrypt=self.cc.Decrypt(ciphertext, self.private)
-        precision=1 # tells us the decimal precision of our resulting values
-        decrypt.GetFormattedValues(precision) # format the decrypted values to proper decimal point
-        vals=decrypt.GetRealPackedValue() # extract the plaintext values and put them in a list
-        # vals=remove_padding(vals, start_len)
-        final=[]
-        for i in vals:
-            rounded=round(i, 1)
-            final.append(rounded) # round the extracted values to one decimal place
-        return final
-
-
            
 @pytest.mark.parametrize("model, plaintext", 
 [
@@ -158,21 +32,18 @@ class OpenFHEScheme(FHEScheme):
 ], 
 ids=["OpenFHE floats", "OpenFHE ints"])
 def test_enc_dec_nums_ofhe(model, plaintext):
-    ofhe_scheme=OpenFHEScheme() # create OpenFHEScheme object
+    ofhe_scheme=fhe_schemes.OpenFHE() # create OpenFHEScheme object
 
     c1 = ofhe_scheme.encrypt(plaintext) # encrypt the plaintext object using the public key
     dec = ofhe_scheme.decrypt(c1) # decrypt using the secret key
     assert np.all(plaintext == dec) #np.all checks if array elements are the same
 
-@pytest.mark.skip(reason="Debug")
+
 @pytest.mark.parametrize("model, plaintext",
 [(ofhe, str1), (ofhe, str2)], ids=["OpenFHE short string", "OpenFHE magic words"])
 def test_enc_dec_str_ofhe(model, plaintext):
-    # plain_t=nearest_power_padding(plaintext, len(plaintext)) # use padding to ensure the string length is a power of two
-    # encoded_plain=[(byte) for byte in plain_t.encode("utf-8")] # encode string characters into bytes using utf-8 format
-    # cc = setup_ofhe_ckks(len(encoded_plain)) # create CKKS parameters
-    ofhe_scheme=OpenFHEScheme() # create scheme, encrypt and decrypt as you would with number input
-    # private, public = ofhe_scheme.getKeys() 
+    ofhe_scheme=fhe_schemes.OpenFHE() # create scheme, encrypt and decrypt as you would with number input
+    
     c1 = ofhe_scheme.encrypt(plaintext) 
     dec = ofhe_scheme.decrypt(c1)
     
@@ -212,34 +83,21 @@ def test_int_enc_dec_tfhe(model, plaintext):
 [(tfhe, str1), (tfhe, str2)], ids=["TFHE short string", "TFHE magic words"])
 def test_str_enc_dec_tfhe(model, plaintext):
     tfhe_scheme=fhe_schemes.TFHE()
-    # private, public = tfhe_scheme.getKeys()
-    # bit_list=ints_to_bits(encoded_plain)
     cipher=tfhe_scheme.encrypt(plaintext)
     dec=tfhe_scheme.decrypt(cipher) 
     assert plaintext == dec
 
 
 
-# @pytest.mark.parametrize("model", [tfhe, ofhe], ids=["OpenFHE", "TFHE"])
-# def test_both_schemes_str(model):
-#     plaintext= "encrypt and decrypt"
-#     scheme
-#     if model == ofhe:
-#         plaintext=nearest_power_padding(plaintext, len(plaintext))
-#         cc = setup_ofhe_ckks(len(plaintext))
-#         scheme=OpenFHEScheme(cc)
-#     else:
-#         rng = np.random.RandomState(123)
-#         scheme=TFHE(rng)
-#     encoded_plain=[(byte) for byte in plaintext.encode("utf-8")]
-#     private, public = scheme.getKeys()
-#     bit_list=ints_to_bits(encoded_plain)
-#     dec=[]
-#     for i in bit_list:
-#         cipher=scheme.encrypt(private, np.array(i))
-#         dec_item=scheme.decrypt(p)
+@pytest.mark.parametrize("model", [ofhe, tfhe], ids=["OpenFHE", "TFHE"])
+def test_both_schemes_str(model):
+    plaintext= "encrypt and decrypt"
+    if model == ofhe:
+        scheme=fhe_schemes.OpenFHE()
+    else:
+        scheme=fhe_schemes.TFHE()
+    cipher=scheme.encrypt(plaintext)
+    dec=scheme.decrypt(cipher) 
+    assert plaintext == dec
     
-
-
-#def test_add_ofhe(model, plaintext):
 
